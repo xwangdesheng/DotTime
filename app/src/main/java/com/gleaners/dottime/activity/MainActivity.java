65707d2327c6d7633c9f1b28055c7a315082f5c4
wangdesheng <wangdesheng2017@163.com>
2021-07-15 17:26:05 +0800

    .

--- app/src/main/java/com/gleaners/dottime/activity/MainActivity.java
+++ app/src/main/java/com/gleaners/dottime/activity/MainActivity.java
@@ -1,22 +1,15 @@
 package com.gleaners.dottime.activity;
 
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentContainerView;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.gleaners.dottime.R;
 import com.gleaners.dottime.base.BaseActivity;
-import com.gleaners.dottime.fragment.AlbumsFragment;
-import com.gleaners.dottime.fragment.ImagesFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,10 +18,6 @@ public class MainActivity extends BaseActivity {
 
     @BindView(R.id.bottomNavigationView)
     BottomNavigationView bottomNavigationView;
-    @BindView(R.id.fragmentContainerView)
-    FragmentContainerView fragmentContainerView;
-
-    private List<Fragment> fragments = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +26,8 @@ public class MainActivity extends BaseActivity {
 
         ButterKnife.bind(this);
 
-        initBottomNavigation();
-
-    }
-
-    private void initBottomNavigation() {
-        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.menu_images:
-                    break;
-                case R.id.menu_albums:
-                    break;
-            }
-            return false;
-        });
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
